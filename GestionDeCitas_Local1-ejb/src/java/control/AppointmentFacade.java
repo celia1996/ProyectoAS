6ab241f99a6e7e00a416a54950698388e6854194
celii <celii@DESKTOP-UV15QPN>
2020-06-01 15:47:43 +0100

    Data Layer : packages entities and control added

---
+++ GestionDeCitas_Local1-ejb/src/java/control/AppointmentFacade.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package control;
+
+import entities.Appointment;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author Celia Navarro Pérez
+ */
+@Stateless
+public class AppointmentFacade extends AbstractFacade<Appointment> {
+
+    @PersistenceContext(unitName = "GestionDeCitas_Local1-ejbPU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public AppointmentFacade() {
+        super(Appointment.class);
+    }
+    
+}
---
+++ GestionDeCitas_Local1-ejb/src/java/control/SystemuserFacade.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package control;
+
+import entities.Systemuser;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author Celia Navarro Pérez
+ */
+@Stateless
+public class SystemuserFacade extends AbstractFacade<Systemuser> {
+
+    @PersistenceContext(unitName = "GestionDeCitas_Local1-ejbPU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public SystemuserFacade() {
+        super(Systemuser.class);
+    }
+    
+}
